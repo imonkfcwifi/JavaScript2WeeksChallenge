3baffe49fc949134ba0928cc30daaba98684305e
Roh <nohyungkorea@gmail.com>
2022-09-02 19:55:04 +0900

    hava fun

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-const animation = document.querySelector(".nah");
+const widthControl = document.body;
 
-function handleAnimation() {
-    if (animation.className === "active") {
-        animation.className = "";
+const violet = "violet";
+const yellow = "yellow";
+const blue = "blue";
+
+function handleControlAnimation() {
+    const inner = window.innerWidth;
+
+    if (inner > 1400) {
+        widthControl.classList.add(violet);
+        widthControl.classList.remove(yellow);
+
     }
+
+    else if (inner <= 1400 && inner > 500) {
+        widthControl.classList.add(yellow);
+        widthControl.classList.remove(violet, blue);
+    }
+
     else {
-        animation.className = "active";
+        widthControl.classList.add(blue);
+        widthControl.classList.remove(yellow);
     }
 }
 
-animation.addEventListener("click", handleAnimation);
+window.addEventListener("resize", handleControlAnimation);
 
