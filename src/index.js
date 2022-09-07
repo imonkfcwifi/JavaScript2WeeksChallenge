5f9c1ec69ea39d7b0017c060163681aee84e0682
Roh <nohyungkorea@gmail.com>
2022-09-07 21:02:56 +0900

    im never gonna give you up

--- src/index.js
+++ src/index.js
@@ -1,41 +1,24 @@
-const LottoForm = document.querySelector(".LottoForm");
-const LottoInput = document.querySelector(".LottoInput");
-const DestinyForm = document.querySelector(".DestinyForm");
-const DestinyInput = document.querySelector(".DestinyInput");
-const result = document.querySelector(".result");
+const timerCount = document.querySelector(".timer");
+const rick = document.querySelector("#rick");
+function day() {
+    const XmasDay = new Date("2022-12-25");
+    const toDay = new Date();
 
-function handlerRangeInput() {
-    const rangeNum = LottoInput.value;
-    DestinyInput.setAttribute("max", rangeNum);
-    // max로 최대
+    const dDay = XmasDay - toDay;
+
+    const dDayDay = Math.floor(dDay / (1000 * 60 * 60 * 24));
+    const dDayHour = Math.floor((dDay / (1000 * 60 * 60)) % 24);
+    const dDayMin = Math.floor((dDay / (1000 * 60)) % 60);
+    const dDaySec = Math.floor(dDay / 1000 % 60);
+
+    timerCount.innerText = `${dDayDay}일 ${dDayHour}시간 ${dDayMin}분 ${dDaySec} 초 남았습니다.`
+
+    const dDayDayrick = Math.floor((dDay / (1000 * 60 * 24)));
+    const dDayHourrick = Math.floor((dDay / (1000 * 60)));
+    const dDayMinrick = Math.floor((dDay / (1000 * 60 * 60 * 60)));
+
+    const hap = dDayDayrick + dDayHourrick + dDayMinrick;
+    rick.innerHTML = `대략 남은 릭 롤링 횟수 ${hap} 회`
 }
-
-LottoForm.addEventListener("input", handlerRangeInput);
-// 입력(숫자나 문자) 즉시 (input) eventlisten
-function handlerSubmit(event) {
-    event.preventDefault();
-    const rangeNum = LottoInput.value;
-    const chooseNum = DestinyInput.value;
-    const randomNum = Math.floor(Math.random() * rangeNum + 1);
-    paintResult(chooseNum, randomNum)
-}
-
-DestinyForm.addEventListener("submit", handlerSubmit);
-// 눌렀을때 (submit) eventlisten
-
-function paintResult(chooseNum, randomNum) {
-
-    if (parseInt(chooseNum) === randomNum) {
-        result.innerText = `You chose: ${chooseNum}, 
-        the machine chose: ${randomNum} You win`
-    }
-
-    else if (chooseNum < 0, randomNum < 0) {
-        result.innerText = `write the right number`
-    }
-    else {
-        result.innerText = `You chose: ${chooseNum}, 
-        the machine chose: ${randomNum} You lose the money`
-
-    }
-}
+day();
+setInterval(day, 1000);
